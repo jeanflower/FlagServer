9c187159d4c5aab1bc9a85f2b0a4c02b98134591
Jean Flower <jeanflower@rocketmail.com>
2021-12-12 07:46:06 +0000

    Better collection name

--- src/models/Ad.model.js
+++ src/models/Ad.model.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 let FlagSchema = new Schema({
     flagID: {type: String, required: true, max: 500},
     flagData: {type: String, required: true, max: 32000}, // in JSON format
-}, { collection: 'adverts' });
+}, { collection: 'flags' });
 
 
 // Export the model
